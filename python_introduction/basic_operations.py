670da74bc2bf594539fbb58e286a316bd75b0325
KvngNii <barneyhamms77@gmail.com>
2025-11-12 14:07:21 +0000

    Add basic operations for addition, subtraction, and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1=10
+number2=5
+add_result=number1+number2
+subtr_result=number1-number2
+mult_result=number1*number2
+
+print("the addition of", number1 ,"and", number2, "is", add_result)
+print("the subtraction of", number1 ,"and", number2, "is", subtr_result)
+print("the multiplication of", number1 ,"and", number2, "is", mult_result)
